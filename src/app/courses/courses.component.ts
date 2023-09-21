dd79ab77bceb62182bde742e5661fb1bbf7253c7
abdelra0394 <abdlramoh394@gmail.com>
2023-09-21 20:28:48 +0300

    Displaying courses List and the selected course

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -13,12 +13,24 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'JavaScript Fundamentals',
+      description: 'Learn the fundamentals of JavaScript',
+      percentComplete: 46,
+      favorite: true
     }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedCourse = null;
+  SelectCourse(course){
+    this.selectedCourse = course;
   }
 
 }
